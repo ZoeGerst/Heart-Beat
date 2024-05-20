4f4e0d6e0ddb900e4a19d8150e1c19e0c98a5ac7
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2024-05-20 11:24:47 -0700

    Add files via upload

--- Assets/Scripts/cerb_health.cs
+++ Assets/Scripts/cerb_health.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class cerb_health : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int currHealth;
+    public int maxHealth = 1000;
+    public int currCerbHealth;
     public cerb_health_bar cerbHealthBar;
     // Start is called before the first frame update
     void Start()
     {
-        currHealth = maxHealth;
+
+        currCerbHealth = maxHealth;
         cerbHealthBar.CerbSetHealth(maxHealth);
     }
 
     // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        //put in trigger
+        if (other.gameObject.tag == "damage")
+        {
+            currCerbHealth -= (10);
+            Debug.Log("ouch: " + gameObject.name);
+            cerbHealthBar.CerbSetHealth(currCerbHealth);
+
+            if (currCerbHealth <= 0)
+            {
+                Debug.Log("trigger");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+    void update()
+    {
+        if (currCerbHealth <= 0)
+        {
+            Debug.Log("trigger");
+        }
     }
 }
